3144ad562d5c1267d6ee2423e79adf67639e91bb
Arturo Fornés Arvayo <fornesarturo@gmail.com>
2019-11-25 14:48:43 -0600

    Remove all Expenses related to the deleted Tag before deleting it

--- common/models/tag.js
+++ common/models/tag.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function(Tag) {
-
+    Tag.observe('before delete', function (ctx, next) {
+        console.log('Going to delete %s matching %j',
+            ctx.Model.pluralModelName,
+            ctx.where);
+        if (ctx.where.id != null) {
+            const Expense = Tag.app.loopback.getModel("Expense");
+            Expense.destroyAll({ tagId: ctx.where.id }, function (err, res) {
+                if (err) return next(err);
+                next();
+            })
+        } else {
+            next({ status: 400, message: 'No multiple deletes allowed' });
+        }
+    });
 };
